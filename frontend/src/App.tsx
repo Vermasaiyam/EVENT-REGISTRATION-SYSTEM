7e092bc5b6c1bdee47b40f362bb8927b2212e5be
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-02 12:05:59 +0530

    main layout page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,98 @@
-import { Button } from './components/ui/button'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import MainLayout from './MainLayout'
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element:
+      // <ProtectedRoutes>
+      <MainLayout />,
+      // </ProtectedRoutes>,
+    children: [
+    //   {
+    //     path: "/",
+    //     element: <LandingPage />
+    //   },
+    //   {
+    //     path: "/profile",
+    //     element: <Profile />
+    //   },
+    //   {
+    //     path: "/search/:id",
+    //     element: <SearchPage />
+    //   },
+    //   {
+    //     path: "/restaurant/:id",
+    //     element: <RestaurantPage />
+    //   },
+    //   {
+    //     path: "/cart",
+    //     element: <Cart />
+    //   },
+    //   {
+    //     path: "/order/status",
+    //     element: <Success />
+    //   },
+    //   {
+    //     path: "/restaurants",
+    //     element: <AllRestaurants />
+    //   },
+    //   //admin
+    //   {
+    //     path: "/admin/restaurant",
+    //     element: <AdminRoute><Restaurant /></AdminRoute>,
+    //   },
+    //   {
+    //     path: "/admin/menu",
+    //     element: <AdminRoute><AddMenu /></AdminRoute>,
+    //   },
+    //   {
+    //     path: "/admin/orders",
+    //     element: <AdminRoute><Orders /></AdminRoute>,
+    //   },
+
+      // Under construction page
+      {
+    path: '*',
+    element: <UnderConstruction />
+  },
+]
+  },
+  // {
+  //   path: "/login",
+  //   element: <AuthenticatedUser><Login /></AuthenticatedUser>,
+  // },
+  // {
+  //   path: "/signup",
+  //   element: <AuthenticatedUser><Signup /></AuthenticatedUser>,
+  // },
+  // {
+  //   path: "/forgot-password",
+  //   element: <AuthenticatedUser><ForgotPassword /></AuthenticatedUser>,
+  // },
+  // {
+  //   path: '/reset-password',
+  //   element: <ResetPassword />
+  // },
+  // {
+  //   path: '/verify-email',
+  //   element: <VerifyEmail />,
+  // },
+  // // Under construction page
+  // {
+  //   path: '*',
+  //   element: <UnderConstruction />
+  },
+])
 
 function App() {
 
   return (
     <>
-      <Button>Click me</Button>
+      <main>
+        <RouterProvider router={appRouter}></RouterProvider>
+      </main>
     </>
   )
 }
