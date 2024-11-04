acbc030acef27a4eebaad7de50f8658b74111710
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-04 23:38:17 +0530

    event model

---
+++ backend/models/event.model.ts
@@ -0,0 +1,71 @@
+import mongoose, { Document } from "mongoose";
+
+export interface IEvent {
+    name: string;
+    description: string;
+    mode: "Online" | "Offline";
+    registrationFee: number;
+    registrationEndDate: Date;
+    eventStartDate: Date;
+    eventEndDate: Date;
+    startTime: string;
+    endTime: string;
+    image?: string;
+    formLink: string;
+}
+
+export interface IEventDocument extends IEvent, Document {
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const eventSchema = new mongoose.Schema<IEventDocument>({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    mode: {
+        type: String,
+        enum: ["Online", "Offline"],
+        required: true,
+    },
+    registrationFee: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    registrationEndDate: {
+        type: Date,
+        required: true,
+    },
+    eventStartDate: {
+        type: Date,
+        required: true,
+    },
+    eventEndDate: {
+        type: Date,
+        required: true,
+    },
+    startTime: {
+        type: String,
+        required: true,
+    },
+    endTime: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: false,
+    },
+    formLink: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+export const Event = mongoose.model("Event", eventSchema);
