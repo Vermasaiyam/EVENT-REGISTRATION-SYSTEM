a2d675eaa54a3f88521be89c626defd5c1912cb0
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-06 10:18:21 +0530

    club page

--- frontend/src/components/ClubPage.tsx
+++ frontend/src/components/ClubPage.tsx
@@ -4,15 +4,16 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import ActiveEvent from "./ActiveEvent";
 import PastEvent from "./PastEvents";
+import { useClubStore } from "@/store/useClubStore";
 
 
 const ClubPage = () => {
 
     const params = useParams();
-    // const { singleClub, getSingleClub } = useClubStore();
+    const { singleClub, getSingleClub } = useClubStore();
 
     useEffect(() => {
-        // getSingleClub(params.id!);
+        getSingleClub(params.id!);
     }, [params.id]);
 
 
@@ -21,8 +22,8 @@ const ClubPage = () => {
             <div className="w-full mx-2">
                 <div className="relative w-full h-32 md:h-64 lg:h-72 ">
                     <img
-                        // src={singleClub?.imageUrl || "Loading..."}
-                        src="https://technovate-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2Fabab2fc5c170491f8277d3ad46a39abc%2Fassets%2Ffavicon%2F761.jpeg&w=1440&q=75"
+                        src={singleClub?.imageUrl || "Loading..."}
+                        // src="https://technovate-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2Fabab2fc5c170491f8277d3ad46a39abc%2Fassets%2Ffavicon%2F761.jpeg&w=1440&q=75"
                         alt="Club Cover Image"
                         className="object-cover w-full h-full rounded-lg shadow-lg"
                     />
@@ -30,12 +31,12 @@ const ClubPage = () => {
                 <div className="flex flex-col md:flex-row justify-between">
                     <div className="my-5">
                         <h1 className="font-bold text-2xl">
-                            {/* {singleClub?.clubName || "Loading..."} */}
-                            DataVerse
+                            {singleClub?.clubName || "Loading..."}
+                            {/* DataVerse */}
                         </h1>
                         <div className="flex gap-2 my-2 flex-wrap">
-                            {/* {singleClub?.eventType.map((cuisine: string, idx: number) => ( */}
-                            {["Hackathons", "Workshops", "KT Sessions", "Coding Competitions", "Tech Quizzes"].map((event: string, idx: number) => (
+                            {/* {["Hackathons", "Workshops", "KT Sessions", "Coding Competitions", "Tech Quizzes"].map((event: string, idx: number) => ( */}
+                            {singleClub?.eventTypes.map((event: string, idx: number) => (
                                 <Badge key={idx}>{event}</Badge>
                             ))}
                         </div>
@@ -60,8 +61,8 @@ const ClubPage = () => {
                             Core Team
                         </h1>
                         <div className="flex gap-2 my-2 flex-wrap">
-                            {["Piyush Pandey", "Amritansh Tripathi", "Shally Agarwal"].map((event: string, idx: number) => (
-                                <Badge variant={"secondary"} key={idx}>{event}</Badge>
+                            {singleClub?.coreTeam.map((member: string, idx: number) => (
+                                <Badge variant={"secondary"} key={idx}>{member}</Badge>
                             ))}
                         </div>
                     </div>
