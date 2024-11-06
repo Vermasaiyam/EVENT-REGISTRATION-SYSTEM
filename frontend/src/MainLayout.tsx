b4fde890e7c8e7289f3e9b12bcdf1c55fe637912
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-07 00:56:34 +0530

    header transition

--- frontend/src/MainLayout.tsx
+++ frontend/src/MainLayout.tsx
@@ -1,26 +1,49 @@
-import { Outlet } from "react-router-dom"
-// import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const MainLayout = () => {
+    const [showNavbar, setShowNavbar] = useState(true);
+    const [lastScrollPos, setLastScrollPos] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY;
+
+            if (currentScrollPos < lastScrollPos) {
+                setShowNavbar(true);
+            } else {
+                setShowNavbar(false);
+            }
+            
+            setLastScrollPos(currentScrollPos);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollPos]);
+
     return (
         <div className="flex flex-col min-h-screen">
-            <header>
+            <header className={`fixed w-full transition-transform duration-300 ${showNavbar ? 'translate-y-0' : '-translate-y-full'} z-20`}>
                 <Navbar />
             </header>
-            {/* Main content  */}
-            <div className="flex-1">
+            
+            {/* Offset for fixed header height */}
+            <div className="pt-[64px] flex-1">
                 <Outlet />
             </div>
 
-            {/* Footer  */}
+            {/* Footer */}
             <footer>
                 <Footer />
             </footer>
         </div>
-    )
-}
+    );
+};
 
-export default MainLayout
+export default MainLayout;
