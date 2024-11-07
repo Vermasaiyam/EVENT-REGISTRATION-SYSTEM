77fd65073a63dc9f19307bf290c8adc384742646
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-07 20:50:44 +0530

    delete event route

--- backend/routes/event.route.ts
+++ backend/routes/event.route.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import upload from "../middlewares/multer";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
-import { addEvent, editEvent } from "../controller/event.controller";
+import { addEvent, deleteEvent, editEvent } from "../controller/event.controller";
 
 const router = express.Router();
 
 router.route("/").post(isAuthenticated, upload.single("image"), addEvent);
 router.route("/:id").put(isAuthenticated, upload.single("image"), editEvent);
+router.route("delete/:id").delete(isAuthenticated, deleteEvent);
 
 export default router;
