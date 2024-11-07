2bed52355f376f0b80070f7f3d9a8ae06af31b83
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-07 10:27:29 +0530

    all admins page

--- frontend/src/head/AllAdmins.tsx
+++ frontend/src/head/AllAdmins.tsx
@@ -1,5 +1,5 @@
-import { useUserStore } from "@/store/useUserStore"
-import moment from 'moment'
+import { useUserStore } from "@/store/useUserStore";
+import moment from "moment";
 import { useEffect, useState } from "react";
 import { MdModeEdit } from "react-icons/md";
 import {
@@ -9,12 +9,11 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 import ChangeUserRole from "./ChangeUserRole";
 
-
 const AllAdmins = () => {
-    const [openUpdateRole, setOpenUpdateRole] = useState(false)
+    const [openUpdateRole, setOpenUpdateRole] = useState(false);
     const [updateUserDetails, setUpdateUserDetails] = useState({
         fullname: "",
         email: "",
@@ -23,75 +22,82 @@ const AllAdmins = () => {
         branch: "",
         admin: false,
         _id: ""
-    })
-
+    });
+    const [adminUsers, setAdminUsers] = useState<any[]>([]);
     const { allUsers, fetchAllUsers } = useUserStore();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetchAllUsers();
-        console.log(allUsers);
-    }, [])
+        const loadUsers = async () => {
+            await fetchAllUsers();
+            setLoading(false);
+        };
+        loadUsers();
+    }, [fetchAllUsers]);
+
+    useEffect(() => {
+        if (allUsers) {
+            setAdminUsers(allUsers.filter(user => user.admin === true));
+        }
+    }, [allUsers]);
 
     return (
-        <div className='bg-white my-4'>
-            <Table>
-                <TableHeader>
-                    <TableRow className="bg-black hover:bg-black dark:bg-white dark:hover:bg-white">
-                        <TableHead className="w-[100px] font-bold text-white">S.No.</TableHead>
-                        <TableHead className="dark:text-black font-bold text-white">Name</TableHead>
-                        <TableHead className="dark:text-black font-bold text-white">Email</TableHead>
-                        <TableHead className="dark:text-black font-bold text-white">Contact Number</TableHead>
-                        <TableHead className="dark:text-black font-bold text-white">Addmission Number</TableHead>
-                        <TableHead className="dark:text-black font-bold text-white">Branch</TableHead>
-                        <TableHead className="dark:text-black font-bold text-white">Admin</TableHead>
-                        <TableHead className="dark:text-black font-bold text-white">Created Date</TableHead>
-                        <TableHead className="dark:text-black font-bold text-white">Edit</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {
-                        allUsers?.map((el, index) => {
-                            return (
-                                <TableRow key={index} className="dark:bg-black dark:hover:bg-black bg-white hover:bg-white">
-                                    <TableCell className="font-medium">{index + 1}</TableCell>
-                                    <TableCell>{el?.fullname}</TableCell>
-                                    <TableCell>{el?.email}</TableCell>
-                                    <TableCell>{el?.contact}</TableCell>
-                                    <TableCell>{el?.addmission_no}</TableCell>
-                                    <TableCell>{el?.branch}</TableCell>
-                                    <TableCell>{el?.admin ? "Yes" : "No"}</TableCell>
-                                    <TableCell>{moment(el?.createdAt).format('LL')}</TableCell>
-                                    <TableCell>
-                                        <button className='bg-green-100 p-2 rounded-full cursor-pointer hover:bg-green dark:hover:bg-[#2E3A52] hover:text-white'
-                                            onClick={() => {
-                                                setUpdateUserDetails(el)
-                                                setOpenUpdateRole(true)
-                                            }}
-                                        >
-                                            <MdModeEdit />
-                                        </button>
-                                    </TableCell>
-                                </TableRow>
-                            )
-                        })
-                    }
-                </TableBody>
-            </Table>
-
-            {
-                openUpdateRole &&
-                (
-                    <ChangeUserRole
-                        onClose={() => setOpenUpdateRole(false)}
-                        fullname={updateUserDetails.fullname}
-                        email={updateUserDetails.email}
-                        admin={updateUserDetails.admin}
-                        userId={updateUserDetails._id}
-                    />
-                )
-            }
+        <div className="bg-white my-4">
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <Table>
+                    <TableHeader>
+                        <TableRow className="bg-black hover:bg-black dark:bg-white dark:hover:bg-white">
+                            <TableHead className="w-[100px] font-bold text-white">S.No.</TableHead>
+                            <TableHead className="dark:text-black font-bold text-white">Name</TableHead>
+                            <TableHead className="dark:text-black font-bold text-white">Email</TableHead>
+                            <TableHead className="dark:text-black font-bold text-white">Contact Number</TableHead>
+                            <TableHead className="dark:text-black font-bold text-white">Admission Number</TableHead>
+                            <TableHead className="dark:text-black font-bold text-white">Branch</TableHead>
+                            <TableHead className="dark:text-black font-bold text-white">Admin</TableHead>
+                            <TableHead className="dark:text-black font-bold text-white">Created Date</TableHead>
+                            <TableHead className="dark:text-black font-bold text-white">Edit</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {adminUsers?.map((el, index: number) => (
+                            <TableRow key={index} className="dark:bg-black dark:hover:bg-black bg-white hover:bg-white">
+                                <TableCell className="font-medium">{index + 1}</TableCell>
+                                <TableCell>{el?.fullname}</TableCell>
+                                <TableCell>{el?.email}</TableCell>
+                                <TableCell>{el?.contact}</TableCell>
+                                <TableCell>{el?.addmission_no}</TableCell>
+                                <TableCell>{el?.branch}</TableCell>
+                                <TableCell>{el?.admin ? "Yes" : "No"}</TableCell>
+                                <TableCell>{moment(el?.createdAt).format("LL")}</TableCell>
+                                <TableCell>
+                                    <button
+                                        className="bg-green-100 p-2 rounded-full cursor-pointer hover:bg-green dark:hover:bg-[#2E3A52] hover:text-white"
+                                        onClick={() => {
+                                            setUpdateUserDetails(el);
+                                            setOpenUpdateRole(true);
+                                        }}
+                                    >
+                                        <MdModeEdit />
+                                    </button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            )}
+            {openUpdateRole && (
+                <ChangeUserRole
+                    onClose={() => setOpenUpdateRole(false)}
+                    fullname={updateUserDetails.fullname}
+                    email={updateUserDetails.email}
+                    admin={updateUserDetails.admin}
+                    userId={updateUserDetails._id}
+                />
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default AllAdmins
+export default AllAdmins;
