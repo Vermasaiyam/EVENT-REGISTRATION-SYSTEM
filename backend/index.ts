cba2b935ced5caa48fae919d07a70cbab78ac613
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-10 22:26:25 +0530

    origin changes

--- backend/index.ts
+++ backend/index.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }
 app.use(cors(corsOptions));
